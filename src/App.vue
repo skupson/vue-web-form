ac9826dd61aa76b88afc383c474f3b39b720aeaf
skupson <andrijaskupinski97@gmail.com>
2020-12-06 19:56:24 +0100

    Init state of the Vue.js app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupForm from "./components/SignupForm.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { SignupForm },
+};
 </script>
 
 <style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <label for="email">Email: </label>
+    <input type="email" required />
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
